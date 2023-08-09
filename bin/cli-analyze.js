77331012eb6bafc456065648490a2fd9812b72e6
jeffery_9 <jeffery_9@163.com>
2023-08-10 01:23:58 +0800

    feat:加入命令行参数及其传递和处理

--- bin/cli-analyze.js
+++ bin/cli-analyze.js
@@ -4,14 +4,25 @@
 
 const path = require('path');
 const { spawn } = require('child_process');
-
+const program = require('commander');
 // 获取当前文件的上级目录
 const currentDir = path.resolve(__dirname, '..');
+//获取被分析目录（使用本工具的目录）
+const anaDir = process.cwd();
 
+program
+  .option('--depth <n>', '设置深度')
+  .option('--json <file-path>', '设置JSON文件路径')
+  .parse(process.argv);
 // 启动子进程执行命令
 const child = spawn(process.platform === 'win32' ? 'npm.cmd' : 'npm', ['run', 'analyze'], {
   cwd: currentDir,
-  stdio: 'inherit'
+  stdio: 'inherit',
+  env: {//把option保存到环境变量中
+    ...process.env,
+    CLI_ARGV: JSON.stringify(process.argv.slice(2)),
+    ANA_DIR: anaDir,
+  }
 });
 
 // 监听子进程的退出事件
--- server/dist/server.js
+++ server/dist/server.js
@@ -2,14 +2,37 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Koa = require("koa");
 const Router = require("koa-router");
-const path = require('path');
-const serve = require('koa-static');
+const path = require("path");
+const serve = require("koa-static");
 const app = new Koa();
 const router = new Router();
+//获取环境变量中的参数
+const anaDir = process.env.ANA_DIR;
+const arr = JSON.parse(process.env.CLI_ARGV);
+// 存储提取出的参数
+const params = {};
+// 遍历数组
+for (let i = 0; i < arr.length; i++) {
+    // 判断元素是否以双横线开头（参数名称）
+    if (arr[i].startsWith('--')) {
+        const paramName = arr[i].slice(2); // 去除双横线前缀
+        const paramValue = arr[i + 1];
+        params[paramName] = paramValue;
+        i++;
+    }
+}
+// 解构赋值保存参数到变量中
+const { depth, json } = params;
+console.log(depth, json);
 // app.use(async ctx => {
-//   ctx.body = 'Hello World';
+//     const depthIndex = process.argv.indexOf("--depth");
+//     const jsonIndex = process.argv.indexOf("--json");
+//     const depth = depthIndex !== -1 ? process.argv[depthIndex + 1] : null;
+//     const json = jsonIndex !== -1;
+//     // ctx.body = `depth: ${depth}, json: ${json}`;
+//     ctx.body = process.argv;
 // });
-app.use(serve(path.join(__dirname, '../public')));
+// app.use(serve(path.join(__dirname, "../public")));
 app.listen(3000);
 console.log("Service started:  http://localhost:3000");
 //# sourceMappingURL=server.js.map
