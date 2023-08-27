e06a25891fe0f34d16e61d7497c10302e6564d5c
jeffery_9 <jeffery_9@163.com>
2023-08-27 15:09:35 +0800

    feat：格式化json

--- server/src/exportJson.ts
+++ server/src/exportJson.ts
@@ -2,6 +2,6 @@ import fs from "fs";
 import path from "path";
 export const exportJson = (obj: object, outpath: string, filename: string) => {
     const op = path.join(outpath, filename);
-    const json = JSON.stringify(obj);
+    const json = JSON.stringify(obj, null, 2);
     fs.writeFileSync(op, json, "utf8");
 };
