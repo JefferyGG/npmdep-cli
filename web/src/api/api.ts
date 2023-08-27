1f1cb717802a4a37d27fb449c926454e7004d232
jeffery_9 <jeffery_9@163.com>
2023-08-27 15:08:56 +0800

    feat：增加获取nodes数据、links数据的api

--- web/src/api/api.ts
+++ web/src/api/api.ts
@@ -1,10 +1,15 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/data/"
+    baseURL: "http://localhost:3000/data/",
 });
   
-export const testJson = async () => {
-    const response = await api.get("/test.json");
+export const nodesJson = async () => {
+    const response = await api.get("/renderNodes.json",);
+    return response.data;
+};
+
+export const linksJson = async () => { 
+    const response = await api.get("/renderLinks.json");
     return response.data;
 };
