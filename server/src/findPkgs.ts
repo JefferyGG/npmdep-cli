a78b763cc92842d71671abb58e0b8abc7fce7eea
jeffery_9 <jeffery_9@163.com>
2023-08-27 19:17:13 +0800

    fix: 采用try-catch语句，修复异常阻断进程问题

--- server/src/findPkgs.ts
+++ server/src/findPkgs.ts
@@ -15,13 +15,19 @@ export const findPkgs = (anadir: string) => {
     
 
     for (const file of files) {
-        const pkg = JSON.parse(fs.readFileSync(file, "utf-8"));
-        if (pkg.name) {
-            names.push(pkg.name);
-            if (pkg.version) {
-                const obj: obj = { name: pkg.name, value:pkg.version };
-                nameVersions.push(obj);
+        try {
+            const pkg = JSON.parse(fs.readFileSync(file, "utf-8"));
+            // 分析处理 pkg 的逻辑...
+            if (pkg.name) {
+                names.push(pkg.name);
+                if (pkg.version) {
+                    const obj: obj = { name: pkg.name, value:pkg.version };
+                    nameVersions.push(obj);
+                }
             }
+        } catch (error) {
+            console.error(`Error processing file ${file}: ${error.message}`);
+            continue; // 跳过当前文件的处理，继续处理下一个文件
         }
         
         
