71875d547ec703d21602f243a993deec0c03b833
jeffery_9 <jeffery_9@163.com>
2023-08-30 23:58:50 +0800

    feat: 增加部分数据展示

--- web/src/api/api.ts
+++ web/src/api/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: "http://localhost:3000/data/",
-    baseURL:"http://127.0.0.1:5173/data"
+    baseURL: "http://localhost:3000/data/",
+    // baseURL:"http://localhost:5173/data"
 });
 
 export const nodesJson = async () => {
@@ -12,5 +12,10 @@ export const nodesJson = async () => {
 
 export const linksJson = async () => { 
     const response = await api.get("/renderLinks.json");
+    return response.data;
+};
+
+export const getFiles = async () => {
+    const response = await api.get("/files.json");
     return response.data;
 };
--- web/src/views/main-chart/a.vue
+++ web/src/views/main-chart/a.vue
@@ -1,11 +1,74 @@
+<script setup lang="ts">
+  import { getFiles, nodesJson } from "../../api/api";
+  import { onMounted, ref } from "vue";
+
+const path = ref([]);
+const node = ref([]);
+onMounted(async () => {
+      try {
+        const filesData = await getFiles();
+        const nodeData = await nodesJson();
+        path.value = filesData;
+        node.value = nodeData;
+        }catch (err){
+      console.log(err);
+      
+    }
+   });
+
+ 
+  
+  
+</script>
 
 <template>
-  <div>页面a 数据统计</div>
-</template>
+  <!-- 展示项目总的包数量 -->
+  <div class="top">
+    <div class="stats shadow">
+      <div class="stat place-items-center">
+        <div class="stat-title">
+          所有package.json数量
+        </div>
+        <div class="stat-value">
+          {{ path.length }}
+        </div>
+        <div class="stat-desc">
+          -
+        </div>
+      </div>
   
-  <script lang="ts">
-  export default {
-      // eslint-disable-next-line vue/component-definition-name-casing, vue/no-reserved-component-names
-      name: "a"
-  };
-  </script>
+      <div class="stat place-items-center">
+        <div class="stat-title">
+          -
+        </div>
+        <div class="stat-value text-secondary">
+          -
+        </div>
+        <div class="stat-desc text-secondary">
+          -
+        </div>
+      </div>
+  
+      <div class="stat place-items-center">
+        <div class="stat-title">
+          -
+        </div>
+        <div class="stat-value">
+          -
+        </div>
+        <div class="stat-desc">
+          -
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.top{
+  height: 150px;
+  width: 100%;
+  display: flex;
+  justify-content: center; 
+}
+</style>
